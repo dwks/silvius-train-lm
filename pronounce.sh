b7576bd6bb0bfb9f7226832cbf99a51e1ede992c
dwk <dwks42@gmail.com>
2018-11-29 05:29:13 +0000

    Support multiple arguments to pronounce.sh.

--- pronounce.sh
+++ pronounce.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Thanks to https://aguo.us/writings/g2p-conversion.html !
 . ../../../tools/env.sh
-echo $@ | g2p.py --model g2p.model --apply - 2>/dev/null
+for word in $@; do
+    echo $word | g2p.py --model g2p.model --apply - 2>/dev/null
+done
